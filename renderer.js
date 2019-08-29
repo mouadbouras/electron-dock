2c1f4e4ac8e404a9e4959dd3421f8bc6c65b0f11
Mouad Bouras <mouad.bouras@randstad.ca>
2019-08-29 16:02:44 -0400

    initial commit

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+//const remote = require('electron'); 
+
+var switchView = require('electron').remote.require('./main').switchView;
+console.log(tabUrl);
+
+function switchTab(tabId, tabUrl)
+{
+    switchView(tabId, tabUrl);
+    console.log(tabUrl);
+}
