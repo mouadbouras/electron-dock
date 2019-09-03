0b5d53864bc866dca9b6b9e8be9d4b61ced3b045
Mouad Bouras <mouad.bouras@randstad.ca>
2019-09-03 17:13:23 -0400

    Remove dist folder

--- dist/model/app-view.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var AppView = /** @class */ (function () {
-    function AppView() {
-    }
-    return AppView;
-}());
-exports.AppView = AppView;
-//# sourceMappingURL=app-view.model.js.map
